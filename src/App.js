4782b6934c4e6f164b361ac504520c75deafaf12
Oleksii Koliukh <oleksii.koliukh@gmail.com>
2023-10-10 14:19:56 +0100

    Single page with search bar returning up to 50 results

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+const baseUrl = 'https://api.discogs.com/database/search?q='
+const apiAuth = '&type=master&key=JsFneGTxouoLwGyyYSiW&secret=qgrIuHbVCUTysFuYwnzDLCmeAjmIeMeG'
 
 function App() {
+
+  const [releases, setReleases] = useState([]);
+  const [releaseSearch, setReleaseSearch] = useState("")
+
+  const handleChange = (event) => {
+    setReleaseSearch(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(baseUrl + releaseSearch + apiAuth)
+    .then((res) => res.json())
+    .then((data) => setReleases(data.results))
+    setReleaseSearch('')
+  }
+
+  const releaseNodes = releases.map((release, index) => {
+    return (
+      <div  className='results'>
+      <div key={index} className='result'>
+        <img src={release.thumb}></img>
+        <h4><a href={`https://www.discogs.com${release.uri}`} target="_blank">{release.title}</a></h4>
+      </div>
+      </div>
+    )
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='release'>Search for a release: </label>
+      <input id='release' type='text' value={releaseSearch} onChange={handleChange}></input>
+      <button type='submit'>Find</button>
+    </form>
+    {releases.length > 0 ? <p>{releases.length + ' results found'}</p> : null}
+    {releaseNodes}
+    </>
   );
 }
 
